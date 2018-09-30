4a2a9b31e4c29cfa3648011138ccc67159709990
weixy <weixy@akulaku.com>
2018-09-30 18:12:52 +0800

    加上cross-env适配不同的开发环境

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,11 +1,17 @@
 var htmlWbpackPlugins = require('html-webpack-plugin'),
     path = require('path'),
     config = require('./config.js'),
-    qs = require('querystring');
+    qs = require('querystring'),
+    fs = require('fs');
+console.log('node_env',process.env.NODE_ENV);
+console.log('script',process.env.npm_lifecycle_script);
+console.log('type',process.env.type);
+// fs.writeFileSync('input.json',JSON.stringify(process.env));
 let projectName = qs.parse(JSON.parse(process.env.npm_config_argv).remain.join('&'))['proj']
 if(!projectName){
     throw new Error('请指定项目名！详情请查阅markdown文件');
 }
+// projectName = 'csr';
 let {entry,template} = config[projectName];
 let baseConfig = {
     mode:'development',
