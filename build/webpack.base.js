df5f0a67a5459dd614de03e442e3f94c38eaa445
weixy <weixy@akulaku.com>
2018-10-10 18:02:56 +0800

    process.argv传参

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -3,14 +3,19 @@ var htmlWbpackPlugins = require('html-webpack-plugin'),
     config = require('./config.js'),
     qs = require('querystring'),
     fs = require('fs');
-console.log('node_env',process.env.NODE_ENV);
-console.log('script',process.env.npm_lifecycle_script);
-console.log('type',process.env.type);
+// console.log('argv',process.argv);
+// console.log('node_env',process.env.NODE_ENV);
+// console.log('script',process.env.npm_lifecycle_script);
+// console.log('type',process.env.type);
 // fs.writeFileSync('input.json',JSON.stringify(process.env));
+
 let projectName = qs.parse(JSON.parse(process.env.npm_config_argv).remain.join('&'))['proj']
 if(!projectName){
     throw new Error('请指定项目名！详情请查阅markdown文件');
 }
+// let args = Array.from(process.argv);
+
+// let projectName = process.argv.includes('')
 // projectName = 'csr';
 let {entry,template} = config[projectName];
 let baseConfig = {
